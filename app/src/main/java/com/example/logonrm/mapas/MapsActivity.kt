7abbf681de4e61a5fa2282fe80558b1f1e10ce97
logonrm <logonrm@fiap.com.br>
2018-03-12 20:47:10 -0300

    Inserindo botao de pesquisa

--- app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
+++ app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.logonrm.mapas
 
+import android.location.Address
+import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -22,28 +25,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        btPesquisar.setOnClickListener {
+            val geocoder = Geocoder(this)
+            var address : List<Address>?
+
+            address = geocoder
+                    .getFromLocationName(
+                            etEndereco.text.toString(),
+                            1)
+            val location = address[0]
+            adicionarMarcador(location.latitude, location.longitude, "Endereço Pesquisar")
+
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    fun adicionarMarcador(latitude: Double, longitude: Double, title: String) {
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(latitude, longitude)
+        mMap.addMarker(MarkerOptions()
+                .position(sydney)
+                .title(title)
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.mipmap.ic_launcher)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f))
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-23.6878954, -46.6917867)
-        mMap.addMarker(MarkerOptions()
-                .position(sydney)
-                .title("Terrara")
-                .icon(BitmapDescriptorFactory
-                    .fromResource(R.mipmap.ic_launcher)))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f))
 
 
     }
