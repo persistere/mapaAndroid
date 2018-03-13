4afa22d9d18efb12def637ab06c2cecd6a52945b
logonrm <logonrm@fiap.com.br>
2018-03-12 22:19:16 -0300

    Inserindo GPS

--- app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
+++ app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.logonrm.mapas
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,10 +19,100 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    val REQUEST_GPS = 0
+
+    override fun onConnected(p0: Bundle?) {
+        checkPermission()
+
+        val minhaLocalizacao = LocationServices
+                .FusedLocationApi
+                .getLastLocation(mGoogleApiClient)
+
+        if (minhaLocalizacao != null)
+        adicionarMarcador(minhaLocalizacao.latitude, minhaLocalizacao.longitude,
+                "Estou aqui!!!")
+    }
+
+    private fun checkPermission() {
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("", "Permissão para gravar negada")
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                val builder = AlertDialog.Builder(this)
+
+                builder.setMessage("Necessária a permissao para GPS")
+                        .setTitle("Permissao Requerida")
+
+                builder.setPositiveButton("OK") { dialog, id ->
+                    requestPermission()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    protected fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_GPS)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_GPS -> {
+                if (grantResults.size == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    //Permissão negada pelo usuário
+                } else {
+                    //Permissao concedida pelo usuario
+                }
+                return
+            }
+        }
+    }
+
+
+    override fun onConnectionSuspended(p0: Int) {
+        Log.i("TAG", "Suspenso")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.i("TAG", "Cancelado")
+    }
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient : GoogleApiClient
+
+    @Synchronized fun callConnection(){
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        mGoogleApiClient.connect()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +123,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         btPesquisar.setOnClickListener {
+
+            mMap.clear()
+
             val geocoder = Geocoder(this)
             var address : List<Address>?
 
@@ -53,8 +151,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 alert.show()
             }
-
-
         }
     }
 
@@ -71,5 +167,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        callConnection()
     }
 }
